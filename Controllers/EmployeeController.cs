2486c73b54f6782bb3a32b81f46210e2194b7bcf
MD AL-AMIN <MD AL-AMIN@ALAMIN785>
2023-09-16 14:27:30 +0600

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using CrudOperatonApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudOperatonApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationContext context;
+        public EmployeeController(ApplicationContext context)
+        {
+            this.context=context;
+        }
+        public IActionResult Index()
+        {
+            var result = context.Employees.ToList();
+            return View(result);
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudOperatonApp.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
